e5226b7822376761ff57de06b2ba5d2ac3197763
Jon Mendizabal <jonmendi@gmail.com>
2024-02-09 16:00:11 +0100

    add home cards with css hover animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import "../styles/home.css";
-import home from "../images/home.png";
-import controller from "../images/controller.png";
+import componentsIso from "../images/components-iso.png";
+import components from "../images/components.png";
+import productsIso from "../images/products-iso.png";
+import products from "../images/products.png";
+import shoppingIso from "../images/shopping-iso.png";
+import shopping from "../images/shopping.png";
 
 export default function Home() {
   return (
     <div id="home">
-      <div className="home-card">
-        <div className="home-image-wrapper">
-          <img src={home} alt="UI" className="home-image" />
+      <div className="home-cards-wrapper">
+        <div className="home-card">
+          <div className="home-image-wrapper">
+            <img src={componentsIso} alt="UI" className="home-image" />
+          </div>
+          <img src={components} alt="" className="card" />
         </div>
-        <img src={controller} alt="" className="controller" />
+        <div className="home-card">
+          <div className="home-image-wrapper">
+            <img src={productsIso} alt="UI" className="home-image" />
+          </div>
+          <img src={products} alt="" className="card" />
+        </div>
+        <div className="home-card">
+          <div className="home-image-wrapper">
+            <img src={shoppingIso} alt="UI" className="home-image" />
+          </div>
+          <img src={shopping} alt="" className="card" />
+        </div>
       </div>
       <h1>
         JON
